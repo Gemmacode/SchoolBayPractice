f132e32e1c3bacbf52af0e0033ecb0c80ab641ba
Gemma <jemmimah.abdul@decagon.dev>
2024-02-28 19:06:52 +0100

    Add project files.

---
+++ SchoolBayApi/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolBayApi.Models
+{
+    public class Student
+    {
+        [Key]
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {MiddleName} {LastName}";
+
+
+
+    }
+}
