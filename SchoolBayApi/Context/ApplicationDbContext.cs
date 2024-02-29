90ec889a7d086b06f0452bd7ebcc97c2cfd22e48
Gemma <jemmimah.abdul@decagon.dev>
2024-02-29 11:50:56 +0100

    Updated

--- SchoolBayApi/Context/ApplicationDbContext.cs
+++ SchoolBayApi/Context/ApplicationDbContext.cs
@@ -1,6 +1,13 @@
-﻿namespace SchoolBayApi.Context
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolBayApi.Models;
+
+namespace SchoolBayApi.Context
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }    
     }
 }
--- SchoolBayApi/Program.cs
+++ SchoolBayApi/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using SchoolBayApi.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("")));
 
 var app = builder.Build();
 
